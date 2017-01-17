16cd1293fe57154d6328e63c6f274bec8c9aae68
evanaaron26 <evanaaron26@yahoo.com>
2017-01-17 14:30:20 -0600

    second prob completed

--- second_problem.rb
+++ second_problem.rb
@@ -3,16 +3,16 @@ def find_string(array)
 total = []
 
 array.each do |string|
-	if string.is_a? Integer
-
+	if string.is_a? String	
 	total << string
 	end
+
 end
 
-	total 
+	total.length
 
 end
 
 #driver code 
 
-p find_string(["hotel", 345, "Joe"])
+p find_string(["hotel", 345, "Joe","hotel", 345, "Joe", "hotel", 345, "Joe"])
